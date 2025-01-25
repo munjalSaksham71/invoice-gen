9a9c947c2d365e0413ae879a1f9eef749170f34b
Abhinandan Goyal <abhinandan@sourcewiz.co>
2025-01-25 13:56:42 +0530

    feat: piechart component

---
+++ src/components/charts/PieChart.tsx
@@ -0,0 +1,42 @@
+import { PieChart , Pie, Cell, ResponsiveContainer } from 'recharts';
+import { map } from 'lodash-es'
+
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+const RADIAN = Math.PI / 180;
+
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent }:any) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
+
+const Pie_Chart = ({ data }: any) => {
+  return (
+  <ResponsiveContainer width="100%" height="100%">
+    <PieChart width={400} height={400}>
+      <Pie
+        data={data}
+        cx="50%"
+        cy="50%"
+        labelLine={false}
+        label={renderCustomizedLabel}
+        outerRadius={80}
+        fill="#8884d8"
+        dataKey="value"
+      >
+        {map(data,( index:number) => (
+          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+        ))}
+      </Pie>
+    </PieChart>
+  </ResponsiveContainer>
+  )
+}
+
+export default Pie_Chart
